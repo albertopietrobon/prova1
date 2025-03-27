3c59c4703bf1484002cc837c5e8340545397ca2d
albertopietrobon <alberto1.pietrobon@mail.polimi.it>
2025-03-27 10:28:21 +0000

    cambio2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import datetime
 
 #Page configuration
 st.set_page_config(
@@ -8,7 +9,7 @@ st.set_page_config(
 )
 
 #Title
-st.title("App: :green[prova 1]")
+st.title("App: :green[prova 1] :cucumber:")
 
 #Header
 st.header("This is a header")
@@ -20,15 +21,37 @@ st.write("### This is a third header")
 st.write("**Welcome** to my new *app*")
 st.write("> this is a blockquote")
 
+#HTML writing
+st.markdown('<p style="color:yellow; font-size:20px;">this is yellow</p>', unsafe_allow_html=True)
+
 #Slider
 data = st.slider("Select a value", 0 , 100)
 st.write("You selected:", data)
 
+st.slider("select a range:", 0, 100, (30,70))
+
 #Button
 sport_list = ["Volleyball", "Football", "Basketball"]
 
-if st.button("Sport", type="primary"):
+if st.button("Sport", type="secondary"):
     st.write(sport_list)
+
+#Checkbox
+if st.checkbox("I like it!"):
+    st.write("Thank you")
+else:
+    st.write("Oh noooo")
 
 #Radio
 st.radio("Which is your favourite sport?", sport_list)
+
+#Selectbox
+chosen=st.selectbox("Choose:", sport_list)
+
+#Multiselect
+chosen1=st.multiselect("Choose your sports:", sport_list)
+
+#Calendar
+
+st.date_input("Select date range:",value=(datetime(2025,3,15),datetime(2025,5,30)))
+
